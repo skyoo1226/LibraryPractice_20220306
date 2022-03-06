8e2ae0ca6c5f8fb2c71d089fc9d87dbbc44c786b
skyoo1226 <you1226@naver.com>
2022-03-06 12:31:14 +0900

    라이브러리 2개 추가 -권한 임시 일부 작업

--- app/src/main/java/com/skyoo/librarypractice_20220306/MainActivity.kt
+++ app/src/main/java/com/skyoo/librarypractice_20220306/MainActivity.kt
@@ -7,5 +7,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+    fun setupEvents() {
+
+    }
+
+
+    
+
+
 }
+// 전화걸기 권한 코드
+// TedPermission.create()
+//        .setPermissionListener(permissionlistener)
+//        .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+//        .setPermissions(Manifest.permission.READ_CONTACTS, Manifest.permission.ACCESS_FINE_LOCATION)
+//        .check();
+
+
